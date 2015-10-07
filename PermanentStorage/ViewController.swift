02f2d796b7472966e41e6088daf98d418a5db482
Bat Computer <Batcomputer@Bats-MacBook-Pro.local>
2015-10-07 15:57:52 -0700

    first commit

--- PermanentStorage/ViewController.swift
+++ PermanentStorage/ViewController.swift
@@ -13,6 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // NSUserDefaults.standardUserDefaults().setObject("Jimmy", forKey: "name")
+        
+        let userName = NSUserDefaults.standardUserDefaults().objectForKey("name")! as! String
+        
+        print(userName)
+        
+        let arr = [1, 2, 3, 4]
+        
+        NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "array")
+        
+        let returnedArray = NSUserDefaults.standardUserDefaults().objectForKey("array")! as! NSArray
+        
+        for x in returnedArray {
+            
+            print(x)
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
